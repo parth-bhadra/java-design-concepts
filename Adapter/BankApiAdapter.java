321083f45cdedd64ac5610496a8b58c28632052d
Parth <parthbhadra14@gmail.com>
2023-09-03 17:14:25 +0530

    adapter, first commit

---
+++ Adapter/BankApiAdapter.java
@@ -0,0 +1,8 @@
+package Adapter;
+
+public interface BankApiAdapter {
+    int getBalance(String userBankAccountId);
+
+
+
+}
---
+++ Adapter/PhonePe.java
@@ -0,0 +1,16 @@
+package Adapter;
+
+public class PhonePe {
+
+    public void getBalance() {
+
+    }
+
+    public void addBankAccount() {
+
+    }
+
+    public void transferAmount() {
+
+    }
+}
